013dc9b4d9ec477548f4d9926f52800d711a1284
David Schach <schachdavid@web.de>
2020-04-19 17:28:41 +0200

    Updates

--- precache-manifest.b891150b6dc761126c93fa3ffcc3bc97.js
+++ precache-manifest.b891150b6dc761126c93fa3ffcc3bc97.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c06ea15b3e9468984082a7096daaca90",
+    "revision": "c52a351cd27e97461470be5ba16faf64",
     "url": "/visualization-perception/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/visualization-perception/static/css/2.c2395193.chunk.css"
   },
   {
-    "revision": "058d6f00ffe8b3a07af4",
-    "url": "/visualization-perception/static/css/main.a7b1c567.chunk.css"
+    "revision": "4d15d1b0250fded8958c",
+    "url": "/visualization-perception/static/css/main.42759bfa.chunk.css"
   },
   {
     "revision": "ee209a57a76bd9cef147",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/visualization-perception/static/js/2.69fba4a9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "058d6f00ffe8b3a07af4",
+    "revision": "4d15d1b0250fded8958c",
     "url": "/visualization-perception/static/js/main.8c882150.chunk.js"
   },
   {
